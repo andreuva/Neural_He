afc47d1c2900008f73c474576f4e30cced898f7a
Andres Vicente <andres.vicente.arevalo@gmail.com>
2022-02-01 09:58:54 +0000

    first working model and spectral compresion script

--- architecture.py
+++ architecture.py
@@ -1,61 +1,106 @@
 import torch
-from torchvision import datasets
-from torchvision import transforms
+# from torchvision import datasets
+# from torchvision import transforms
+import numpy as np
+from random import shuffle
 import matplotlib.pyplot as plt
 from tqdm import tqdm
+import pickle as pkl
 
 # Transforms images to a PyTorch Tensor
-tensor_transform = transforms.ToTensor()
+# tensor_transform = transforms.ToTensor()
+# # Download the MNIST Dataset
+# dataset = datasets.MNIST(root = "./data",
+# 						train = True,
+# 						download = True,
+# 						transform = tensor_transform)
 
-# Download the MNIST Dataset
-dataset = datasets.MNIST(root = "./data",
-						train = True,
-						download = True,
-						transform = tensor_transform)
+# Define the dataset class for storing the data
+class spectral_dataset(torch.utils.data.Dataset):
+    def __init__(self, data_path, train=True, train_split=0.8):
+        # Load the spectral data
+        with open(data_path, 'rb') as f:
+            data = pkl.load(f)
+
+        # Compute the number of samples for the training and test sets
+        self.n_samples = len(data['intensities'])
+        if train:
+            self.n_samples = int(self.n_samples * train_split)
+            start = 0
+        else:
+            self.n_samples = int(self.n_samples * (1 - train_split))
+            start = int(self.n_samples * train_split)
+
+        # create the shuffled indices
+        indices = list(range(self.n_samples))
+        shuffle(indices)
+
+        # Load the samples (separate the training and test data)
+        self.data = np.array(data['intensities'][start:], dtype=np.float32)
+        # Load the labels
+        self.labels = np.concatenate((data['fft_coeffs'][start:].real, data['fft_coeffs'][start:].imag), 
+                                     axis=-1, dtype=np.float32)
+
+        # shuffle the data
+        self.data = self.data[indices]
+        self.labels = self.labels[indices]
+
+        self.n_features = self.data.shape[1]
+        self.n_components = int(self.labels.shape[1]/2)
+
+    def __getitem__(self, index):
+        return self.data[index], self.labels[index]
+
+    def __len__(self):
+        return self.n_samples
+
+# create the training dataset
+dataset = spectral_dataset('data/spectra/model_ready_data.pkl', train=True)
+# create the test dataset
+test_dataset = spectral_dataset('data/spectra/model_ready_data.pkl', train=False)
 
 # DataLoader is used to load the dataset
 # for training
 loader = torch.utils.data.DataLoader(dataset = dataset,
-									batch_size = 32,
-									shuffle = True)
+									 batch_size = 128,
+									 shuffle = True)
 
 # Creating a PyTorch class
 # 28*28 ==> 9 ==> 28*28
 class AE(torch.nn.Module):
-    def __init__(self):
+    def __init__(self, n_components, n_features):
         super().__init__()
+
+        self.n_components = n_components
+        self.n_features = n_features
           
         # Building an linear encoder with Linear
         # layer followed by Relu activation function
-        # 784 ==> 9
+        # n_features ==> 18
         self.encoder = torch.nn.Sequential(
-            torch.nn.Linear(28 * 28, 128),
+            torch.nn.Linear(n_features, 128),
             torch.nn.ReLU(),
             torch.nn.Linear(128, 64),
             torch.nn.ReLU(),
             torch.nn.Linear(64, 36),
             torch.nn.ReLU(),
             torch.nn.Linear(36, 18),
-            torch.nn.ReLU(),
-            torch.nn.Linear(18, 9)
+            torch.nn.ReLU()
         )
           
         # Building an linear decoder with Linear
         # layer followed by Relu activation function
         # The Sigmoid activation function
         # outputs the value between 0 and 1
-        # 9 ==> 784
+        # 18 ==> n_components
         self.decoder = torch.nn.Sequential(
-            torch.nn.Linear(9, 18),
-            torch.nn.ReLU(),
             torch.nn.Linear(18, 36),
             torch.nn.ReLU(),
             torch.nn.Linear(36, 64),
             torch.nn.ReLU(),
             torch.nn.Linear(64, 128),
             torch.nn.ReLU(),
-            torch.nn.Linear(128, 28 * 28),
-            torch.nn.Sigmoid()
+            torch.nn.Linear(128, n_components*2)
         )
   
     def forward(self, x):
@@ -64,30 +109,33 @@ class AE(torch.nn.Module):
         return decoded
 
 # Model Initialization
-model = AE()
-  
+model = AE(dataset.n_components, dataset.n_features)
+
+# Validation using custom loss function
+# def fft_loss(self, out, target):
+#     reconstructed = np.fft.irfft(out, n=self.n_features)
+#     loss = torch.mean((reconstructed - target)**2)
+#     return loss
+# loss_function = fft_loss
+
 # Validation using MSE Loss function
 loss_function = torch.nn.MSELoss()
   
 # Using an Adam Optimizer with lr = 0.1
 optimizer = torch.optim.Adam(model.parameters(),
-                             lr = 1e-1,
-                             weight_decay = 1e-8)
+                             lr = 1e-4)
 
-epochs = 20
-outputs = []
+epochs = 2500
 losses = []
 for epoch in range(epochs):
-    for (image, _) in tqdm(loader, desc = "Epoch {}".format(epoch)):
+    for (spectra, fft_coef) in tqdm(loader, desc = "Epoch {}".format(epoch)):
         
-        # Reshaping the image to (-1, 784)
-        image = image.reshape(-1, 28*28)
-        
+        # Forward pass
         # Output of Autoencoder
-        reconstructed = model(image)
+        reconstructed = model(spectra)
         
         # Calculating the loss function
-        loss = loss_function(reconstructed, image)
+        loss = loss_function(reconstructed, fft_coef)
         
         # The gradients are set to zero,
         # the the gradient is computed and stored.
@@ -98,25 +146,30 @@ for epoch in range(epochs):
         
         # Storing the losses in a list for plotting
         losses.append(loss)
-    outputs.append((epochs, image, reconstructed))
   
 # # Defining the Plot Style
-plt.style.use('fivethirtyeight')
+# plt.style.use('fivethirtyeight')
 plt.xlabel('Iterations')
 plt.ylabel('Loss')
   
 # # Plotting the last 100 values
 plt.plot([los.item() for los in losses])
+plt.xscale('log')
+plt.yscale('log')
 plt.show()
 
-for i, item in enumerate(image):
-    
-    # Reshape the array for plotting
-    item = item.reshape(-1, 28, 28)
-    plt.imshow(item.detach().numpy()[0])
-plt.show()
+# select a random sample from the test dataset and test the network
+# then plot the predicted output and the ground truth
+for indx in np.random.randint(0,test_dataset.n_samples,5):
+    spectra, fft_coef = test_dataset[indx]
 
-for i, item in enumerate(reconstructed):
-    item = item.reshape(-1, 28, 28)
-    plt.imshow(item.detach().numpy()[0])
-plt.show()
+    fft_rec = model.forward(torch.tensor(spectra))
+    fft_rec = fft_rec.detach().numpy()
+    fft_rec_imag = np.zeros(test_dataset.n_components, dtype=np.complex64)
+    fft_rec_imag.real = fft_rec[:test_dataset.n_components]
+    fft_rec_imag.imag = fft_rec[test_dataset.n_components:]
+    reconstructed = np.fft.irfft(fft_rec_imag, n=len(spectra))
+
+    plt.plot(spectra, 'or')
+    plt.plot(reconstructed, 'b')
+    plt.show()
--- spectral_compresion.py
+++ spectral_compresion.py
@@ -104,21 +104,20 @@ and reconstruct the spectra using the fourier coefficients and the frequencies
 """
 def compress_spectra_fft(freq, intensities, n_components):
     # perform the fft
-    fft = np.fft.fft(intensities, axis=1)
+    fft = np.fft.rfft(intensities, axis=1)
     # extract the coefficients
     fft_coeff = fft[:,:n_components]
     # reconstruct the spectra
-    reconstructed = np.fft.ifft(fft_coeff, n=freq.shape[0], axis=1)
-    # reshape the data
-    reconstructed = np.reshape(reconstructed, (reconstructed.shape[0], reconstructed.shape[1]))
+    reconstructed = np.fft.irfft(fft_coeff, n=freq.shape[0], axis=1)
+    
     # normalize the data
-    reconstructed = (reconstructed - np.mean(reconstructed))/np.std(reconstructed)
+    # reconstructed = (reconstructed - np.mean(reconstructed))/np.std(reconstructed)
 
     return fft_coeff, reconstructed
 
 if __name__ == "__main__":
     # load the data from the file
-    freq, intensities = load_data('data/spectra/test_data.pkl')
+    freq, intensities = load_data('data/spectra/raw_data.pkl')
 
     # extract a subsample of the data to test
     np.random.seed(7777)
@@ -130,14 +129,26 @@ if __name__ == "__main__":
     
     # compress the data
     pca_object, reconstructed_pca = compress_spectra_pca(freq, intensities, intensities_test, n_components=15)  
-    poly_models, reconstructed_poly = compress_spectra_poly(freq, intensities_test, 30)
-    spline_models, reconstructed_spline = compress_spectra_spline(freq, intensities_test, 30)
-    fft_coeff, reconstructed_fft = compress_spectra_fft(freq, intensities_test, 50)
+    poly_models, reconstructed_poly = compress_spectra_poly(freq, intensities_test, 20)
+    spline_models, reconstructed_spline = compress_spectra_spline(freq, intensities_test, 20)
+    fft_coeff, reconstructed_fft = compress_spectra_fft(freq, intensities_test, 25)
 
     plot_data(freq, intensities_test, color='.b')
-    plot_data(freq, reconstructed_poly, color='g')
-    plot_data(freq, reconstructed_spline, color='pink')
+    # plot_data(freq, reconstructed_poly, color='g')
+    # plot_data(freq, reconstructed_spline, color='pink')
     plot_data(freq, reconstructed_fft, color='orange')
     plot_data(freq, reconstructed_pca, color='r', show=True)
 
+    # compute the fft coefficients of all the samples in the data
+    fft_coeffs, reconstructed_ffts = compress_spectra_fft(freq, intensities, 25)
     
+    # create a dictionary with the coefficients of the different models
+    # and the instensities that are associated to each model
+    models_dict = {
+        'intensities': intensities,
+        'fft_coeffs': fft_coeffs,
+                  }
+
+    # save the coefficients to a pkl for training the encoder-decoder network
+    # with open('data/spectra/model_ready_data.pkl', 'wb') as f:
+    #     pkl.dump(models_dict, f)
